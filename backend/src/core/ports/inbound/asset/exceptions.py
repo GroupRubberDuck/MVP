bf6148f401ee69928b6af2cac170b02f7904130c
aldobettega <aldo.bettega04@gmail.com>
2026-05-07 15:13:46 +0200

    Feat: scritta porta e servizio di asset anagraphic service più relativi test. #90 #91

--- backend/src/core/ports/inbound/asset/exceptions.py
+++ backend/src/core/ports/inbound/asset/exceptions.py
@@ -5,3 +5,7 @@ class DeleteAssetFailure(Exception):
 class GetAssetDetailFailure(Exception):
     """L'asset detail non può essere recuperato (sessione non trovata o asset non presente)."""
     pass
+
+class GetAssetAnagraphicFailure(Exception):
+    """L'anagrafica dell'asset non può essere recuperata (sessione non trovata o asset non presente)."""
+    pass
