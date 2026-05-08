a30811befe9738d3648f17d8c51708bcaf4e4535
unluckypippo <filippoguerra1310@gmail.com>
2026-05-08 10:38:11 +0200

    feat: creato template device_list e device_detail. #67, #68

--- backend/src/adapters/inbound/device/flask_query_device_controller.py
+++ backend/src/adapters/inbound/device/flask_query_device_controller.py
@@ -1,16 +1,16 @@
 from core.ports.inbound.device.get_device_list_use_case import GetDeviceListUseCase
-from core.ports.inbound.device.get_device_use_case import GetDeviceDetailCommand, GetDeviceDetailUseCase 
+from core.ports.inbound.device.get_device_use_case import GetDeviceDetailCommand, GetDeviceDetailUseCase
 from core.ports.inbound.compliance_standard.get_compliance_standard_use_case import GetComplianceStandardCommand, GetComplianceStandardUseCase
-
 from core.ports.inbound.compliance_standard.exceptions import StandardNotFoundFailure
 from core.ports.inbound.device.exceptions import DeviceNotFoundFailure
-
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, abort
 from pydantic import BaseModel
+
 
 class DeviceListItemDTO(BaseModel):
     device_id: str
     name: str
+
 
 class DeviceDetailDTO(BaseModel):
     device_id: str
@@ -20,18 +20,20 @@ class DeviceDetailDTO(BaseModel):
     compliance_standard_name: str
     compliance_standard_version: str
 
+
 class FlaskQueryDeviceController:
     def __init__(
         self,
         get_device_list_use_case: GetDeviceListUseCase,
         get_device_detail_use_case: GetDeviceDetailUseCase,
-        get_compliance_standard_use_case: GetComplianceStandardUseCase
+        get_compliance_standard_use_case: GetComplianceStandardUseCase,
     ) -> None:
         self._get_device_list_use_case = get_device_list_use_case
         self._get_device_detail_use_case = get_device_detail_use_case
         self._get_compliance_standard_use_case = get_compliance_standard_use_case
 
     def register_routes(self, blueprint: Blueprint) -> None:
+
         @blueprint.route("/devices", methods=["GET"])
         def get_device_list():
             device_list = self._get_device_list_use_case.get_device_list()
@@ -44,25 +46,50 @@ class FlaskQueryDeviceController:
             ]
             return render_template("layouts/device/device_list.html", devices=device_list_dto)
 
-
         @blueprint.route("/devices/<device_id>", methods=["GET"])
         def get_device_detail(device_id):
             command = GetDeviceDetailCommand(device_id=device_id)
             try:
                 device = self._get_device_detail_use_case.get_device_detail(command)
                 standard = self._get_compliance_standard_use_case.get_compliance_standard(
-                        GetComplianceStandardCommand(standard_id=device.standard_id)
+                    GetComplianceStandardCommand(standard_id=device.standard_id)
                 )
-                return render_template("layouts/device/device_detail.html", device=DeviceDetailDTO(
-                    device_id=device.id,
-                    name=device.name,
-                    os=device.os,
-                    description=device.description,
-                    compliance_standard_name=standard.name,
-                    compliance_standard_version=standard.version_number
-                )), 200
-            except (DeviceNotFoundFailure) as e:
+                return render_template(
+                    "layouts/device/device_detail.html",
+                    device=DeviceDetailDTO(
+                        device_id=device.id,
+                        name=device.name,
+                        os=device.os,
+                        description=device.description,
+                        compliance_standard_name=standard.name,
+                        compliance_standard_version=standard.version_number,
+                    ),
+                ), 200
+            except DeviceNotFoundFailure as e:
                 return render_template("error.html", message=str(e)), 404
-            except (StandardNotFoundFailure) as e:  
+            except StandardNotFoundFailure as e:
                 return render_template("error.html", message=str(e)), 404
-            
+
+        @blueprint.route("/devices/new", methods=["GET"])
+        def get_add_device_form():
+            abort(501)
+
+        @blueprint.route("/devices/import", methods=["GET"])
+        def get_import_device_form():
+            abort(501)
+
+        @blueprint.route("/devices/<device_id>/edit", methods=["GET"])
+        def get_update_device_form(device_id):
+            abort(501)
+
+        @blueprint.route("/devices/<device_id>/delete", methods=["POST"])
+        def delete_device(device_id):
+            abort(501)
+
+        @blueprint.route("/devices/<device_id>/export", methods=["GET"])
+        def export_device(device_id):
+            abort(501)
+
+        @blueprint.route("/devices/<device_id>/evaluation/start", methods=["GET"])
+        def start_evaluation_session(device_id):
+            abort(501)
